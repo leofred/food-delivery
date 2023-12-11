96d29297bc74853c5fc1870f9cc2e1af9a680d9d
Leandro Frederico <leandro_frederico@hotmail.com>
2023-12-11 20:39:16 -0300

    graphql setup

--- servers/apps/users/src/users.service.ts
+++ servers/apps/users/src/users.service.ts
@@ -1,8 +1,46 @@
-import { Injectable } from '@nestjs/common';
-
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { LoginDto, RegisterDto } from './dto/user.dto';
+import { PrismaService } from '../../../prisma/prisma.service';
+import { Response } from 'express';
 @Injectable()
 export class UsersService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly prisma: PrismaService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  // register user
+  async register(registerDto: RegisterDto, response: Response) {
+    const { name, email, password } = registerDto;
+    const isEmailExist = await this.prisma.user.findUnique({
+      where: { email },
+    });
+    if (isEmailExist) {
+      throw new BadRequestException('User already exist with this email!');
+    }
+    const user = await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+    });
+    return { user, response };
+  }
+
+  async Login(loginDto: LoginDto /*, response: Response*/) {
+    const { email, password } = loginDto;
+    const user = {
+      email,
+      password,
+    };
+    return user;
+  }
+
+  async getUsers() {
+    return this.prisma.user.findMany({});
   }
 }
